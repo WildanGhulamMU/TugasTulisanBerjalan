e586e61d4fe7df9902bb99e3a23a537e13a8a3ce
WildanGhulamMU <138188108+WildanGhulamMU@users.noreply.github.com>
2023-09-20 12:15:26 +0700

    Add files via upload

---
+++ Tulisan Berjalan/Tulisan Berjalan/Form1.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace Tulisan_Berjalan
+{
+    partial class frmTulisanBerjalan
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lblAnimasi = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.btnkeluar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblAnimasi
+            // 
+            this.lblAnimasi.AutoSize = true;
+            this.lblAnimasi.Location = new System.Drawing.Point(189, 58);
+            this.lblAnimasi.Name = "lblAnimasi";
+            this.lblAnimasi.Size = new System.Drawing.Size(82, 13);
+            this.lblAnimasi.TabIndex = 0;
+            this.lblAnimasi.Text = "Tulisan Berjalan";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // btnkeluar
+            // 
+            this.btnkeluar.Location = new System.Drawing.Point(325, 187);
+            this.btnkeluar.Name = "btnkeluar";
+            this.btnkeluar.Size = new System.Drawing.Size(75, 23);
+            this.btnkeluar.TabIndex = 1;
+            this.btnkeluar.Text = "Keluar";
+            this.btnkeluar.UseVisualStyleBackColor = true;
+            this.btnkeluar.Click += new System.EventHandler(this.btnkeluar_Click);
+            // 
+            // frmTulisanBerjalan
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(427, 231);
+            this.Controls.Add(this.btnkeluar);
+            this.Controls.Add(this.lblAnimasi);
+            this.Name = "frmTulisanBerjalan";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Tulisan Berjalan";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblAnimasi;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button btnkeluar;
+    }
+}
+
